f269ff5f9605c97f081293bdf1569e159dbb8f86
gurpreet562 <kgurpreet562@gmail.com>
2017-10-31 20:46:20 +0530

    Create validator.module.ts

---
+++ validator.module.ts
@@ -0,0 +1,16 @@
+import {
+    NgModule,
+    Component,
+    Pipe,
+    OnInit
+} from '@angular/core';
+import {
+    ReactiveFormsModule,
+    FormsModule,
+    FormGroup,
+    FormControl,
+    Validators,
+    FormBuilder
+} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
